dc6645a241b62de01948103d93c3cefa617fcead
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-16 22:42:29 +0530

    added express server and git ignore file newly

---
+++ express-server.mjs
@@ -0,0 +1,18 @@
+//imporing Express server using ES module
+import express from "express";
+
+//creating server
+const server = express();
+
+//READ method
+server.get("/", (req, res) => {
+  res.send(<h1>Server Created</h1>);
+});
+
+//create a port
+const port = 8000;
+
+//creating a server listener
+server.listen(port, () => {
+  console.log("Server is listening on port : " + port);
+});
