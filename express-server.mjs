265ea27b98974dd1298621c49eaf4475da230e0f
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-17 00:38:00 +0530

    post response added

--- express-server.mjs
+++ express-server.mjs
@@ -1,16 +1,25 @@
 //imporing Express server using ES module
 import express from "express";
 
+//create a port
+const port = 8000;
+
 //creating server
 const server = express();
 
+//creating middleware
+server.use(express.json());
+
 //READ method
 server.get("/", (req, res) => {
-  res.send({ message: "Hellow from server!" });
+  res.send({ message: "Hello from server!" });
 });
 
-//create a port
-const port = 8000;
+//POST method
+server.post("/", (req, res) => {
+  const { body } = req;
+  res.send("<h1>Post method created successfully</h1>"+ `${body}`);
+});
 
 //creating a server listener
 server.listen(port, () => {
