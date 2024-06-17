864c78b5ebb67ae8bf186dfd5ebf4b0bcd12c8e4
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-17 12:14:55 +0530

    PUT and DELETE method performed

--- express-server.mjs
+++ express-server.mjs
@@ -22,6 +22,24 @@ server.post("/", (req, res) => {
   res.send({ message: "Post method created successfully" });
 });
 
+//PUT method
+server.put("/", (req, res) => {
+  const { body } = req;
+  console.log(body);
+
+  res.send({ message: "Put method performed successfully" });
+});
+
+//DELETE method
+server.delete(
+  ("/",
+  (req, res) => {
+    const { body } = req;
+    console.log(body);
+    res.send({ message: "delete method performed well!" });
+  })
+);
+
 //creating a server listener
 server.listen(port, () => {
   console.log("Server is listening on port : " + port + " " + new Date());
